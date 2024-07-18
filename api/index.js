ad05056433d83285e25aa84d93139c946ca1817c
Nishchay Parashar <135508771+Nishchays-github@users.noreply.github.com>
2024-07-18 13:17:01 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(
   cors({
     credentials: true,
-    origin: ["https://deploy-mern-1whq.vercel.app"],
+    origin: ["https://airbnb-clone-client-eight.vercel.app/"],
     methods:["POST" , "GET"]
   })
 );
